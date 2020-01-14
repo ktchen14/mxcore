bcf687c3cffac15966b4ee86cb55431a4ce12777
Kaiting Chen <ktchen14@gmail.com>
2020-01-14 08:57:35 -0800

    More vector_access

--- test/vector_access.c
+++ test/vector_access.c
@@ -2,17 +2,46 @@
 
 #include "../source/vector.h"
 
-int main(int argc __attribute__((unused)), char *argv[] __attribute__((unused))) {
-  int *vector = mx_vector_define(int, 1, 2, 3, 4);
-  int elmt;
+void test_vector_get(void) {
+  int *vector = mx_vector_define(int, 0, 1, 2, 3);
 
-  // It copies the element at the index in the vector to the elmt
-  elmt = 0;
-  mx_vector_get(vector, 1, &elmt, sizeof(int));
+  // It copies the element at the index in the vector to elmt
+  int elmt = 0;
+  mx_vector_get(vector, 2, &elmt, sizeof(int));
   assert(elmt == 2);
 
-  // It copies the elmt to the element at the index in the vector
-  elmt = 9;
-  mx_vector_set(vector, 3, &elmt, sizeof(int));
-  assert(vector[3] == 9);
+  // When elmt is itself an element in the vector, it copies the element at the
+  // index in the vector to elmt
+  mx_vector_get(vector, 3, vector + 1, sizeof(int));
+  assert(vector[1] == 3);
+
+  mx_vector_delete(vector);
+}
+
+void test_vector_set(void) {
+  int *vector = mx_vector_define(int, 0, 1, 2, 3);
+
+  // It copies elmt to the element at the index in the vector. No other elements
+  // in the vector are modified.
+  int elmt = 9;
+  mx_vector_set(vector, 2, &elmt, sizeof(int));
+  assert(vector[0] == 0);
+  assert(vector[1] == 1);
+  assert(vector[2] == 9);
+  assert(vector[3] == 3);
+
+  // When elmt is itself an element in the vector, it copies elmt to the element
+  // at the index in the vector. No other elements in the vector are modified.
+  mx_vector_set(vector, 3, vector + 1, sizeof(int));
+  assert(vector[0] == 0);
+  assert(vector[1] == 1);
+  assert(vector[2] == 9);
+  assert(vector[3] == 1);
+
+  mx_vector_delete(vector);
+}
+
+int main() {
+  test_vector_get();
+  test_vector_set();
 }
