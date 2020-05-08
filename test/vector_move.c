80293a8b0e9766fe06c660e4ea5a7e9918d63e5e
Kaiting Chen <ktchen14@gmail.com>
2020-05-08 11:28:39 -0700

    Refactor vector_move test

--- test/vector_move.c
+++ test/vector_move.c
@@ -9,7 +9,7 @@ void vector_move_z(vector_t vector, size_t target, size_t source, size_t z) {
   REAL(vector_move_z)(vector, target, source, last_z = z);
 }
 
-int main() {
+void test_vector_move(void) {
   int *vector = vector_define(int, 1, 2, 3, 5, 8, 13);
   int number = 0;
 
@@ -27,27 +27,25 @@ int main() {
 
   // It calls vector_move_z() with the element size of the vector
   vector_move(vector, 0, 3);
-  assert(last_z == sizeof(int));
+  assert(last_z == sizeof(vector[0]));
 
   // When the source index is before the target index, it moves the element at
   // the source index to the target index in the vector.
   vector_move(vector, 2, 0);
-  assert(vector[0] == 2);
-  assert(vector[1] == 3);
-  assert(vector[2] == 1);
-  assert(vector[3] == 5);
-  assert(vector[4] == 8);
-  assert(vector[5] == 13);
+  assert_vector_data(vector, 2, 3, 1, 5, 8, 13);
 
   // When the source index is after the target index, it moves the element at
   // the source index to the target index in the vector.
   vector_move(vector, 1, 4);
-  assert(vector[0] == 2);
-  assert(vector[1] == 8);
-  assert(vector[2] == 3);
-  assert(vector[3] == 1);
-  assert(vector[4] == 5);
-  assert(vector[5] == 13);
+  assert_vector_data(vector, 2, 8, 3, 1, 5, 13);
+
+  // When the source and target indices are the same the vector is unmodified
+  vector_move(vector, 3, 3);
+  assert_vector_data(vector, 2, 8, 3, 1, 5, 13);
 
   vector_delete(vector);
+}
+
+int main() {
+  test_vector_move();
 }
