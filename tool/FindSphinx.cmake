41f639dd68d4a03fea8e90c8c00075b9652636f4
Kaiting Chen <ktchen14@gmail.com>
2020-10-22 10:04:18 -0700

    Simplify FindSphinx

--- tool/FindSphinx.cmake
+++ tool/FindSphinx.cmake
@@ -1,20 +1,8 @@
+include(FindPackageHandleStandardArgs)
+
 find_program(SPHINX_EXECUTABLE
   NAMES sphinx-build
   DOC "Sphinx documentation generation tool (http://www.sphinx-doc.org/)")
 mark_as_advanced(SPHINX_EXECUTABLE)
 
-if (SPHINX_EXECUTABLE)
-  execute_process(COMMAND "${SPHINX_EXECUTABLE}" --version
-    OUTPUT_VARIABLE sphinx_version
-    ERROR_QUIET
-    OUTPUT_STRIP_TRAILING_WHITESPACE)
-  if (sphinx_version MATCHES "^sphinx-build [0-9]")
-    string(REPLACE "sphinx-build " "" SPHINX_VERSION_STRING "${sphinx_version}")
-  endif ()
-  unset(sphinx_version)
-endif ()
-
-include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(Sphinx
-  REQUIRED_VARS SPHINX_EXECUTABLE
-  VERSION_VAR SPHINX_VERSION_STRING)
+find_package_handle_standard_args(Sphinx DEFAULT_MSG SPHINX_EXECUTABLE)
